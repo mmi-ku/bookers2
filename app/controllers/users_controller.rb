febb04f7065b4f9e155742278649a51fc4530fbc
mmi-ku <161840539+mmi-ku@users.noreply.github.com>
2024-05-14 11:07:06 +0000

    デザイン途中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
     @book_new = Book.new
-    # ここをどうするか考える！！！！！
+   
   end
 
   def edit
